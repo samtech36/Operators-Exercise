100e252e418e0227436a6c4b6058aea7b47aa026
samtech36 <samuel.hossain@outlook.com>
2024-08-12 13:01:54 -0400

    My Operators Exercise

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -4,7 +4,40 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 15;
+            int b = 21;
+            
+            
+            //addition
+            int sum = a + b;
+            //subtraction
+            int diff = a - b;
+            //multiply
+            int product = a * b;
+            //division
+            int quotient = a / b;
+            //remainder
+            int remainder = a % b;
+            
+            Console.WriteLine($"The sum of {a}+{b} is {sum}");
+            Console.WriteLine($"The difference of {a}-{b} is {diff}");
+            Console.WriteLine($"The product of {a}*{b} is {product}");
+            Console.WriteLine($"The division of {a}/{b} is {quotient}");
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            Console.WriteLine("Enter the radius of the circle: ");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine($"The area of your circle radius is {AreaOfCircle(radius)}");
+            
         }
+
+        public static double AreaOfCircle(double radius)
+        {
+            double area = Math.PI * Math.Pow(radius, 2);
+            return area;
+
+        }
+
+       
+        
     }
 }
